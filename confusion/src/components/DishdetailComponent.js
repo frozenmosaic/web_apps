8fc0183c3f8102537dd18bc9c3a452eae71bc1d7
Jeff Huynh <thaovy.vu.huynh@gmail.com>
2020-06-25 15:17:57 -0400

    dishdetail

---
+++ confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+
+class Dishdetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
+  renderComments(comments) {
+    if (comments != null) {
+        return (
+            <div className="container">
+              <h4>Comments</h4>
+              <ul className="list-unstyled">
+                  {comments.map((c) => {
+                      return (
+                        <li className="p-3">
+                            {c.comment} <br/>
+                            -- {c.author}, {c.date}
+                        </li>
+                      );    
+                  })}
+              </ul>
+            </div>
+          );
+    } else {
+        return <div></div>;
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {this.renderDish(this.props.dish)}
+          </div>
+          <div className="col-12 col-md-5 m-1">{this.renderComments(this.props.dish.comments)}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dishdetail;
